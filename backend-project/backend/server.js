df9043d3418caab12a91178a28de16bdbde0a7f1
francesca.castangia <castangiaf2@gmail.com>
2025-08-25 15:03:07 +0200

    esercizio Change the HTML response

--- backend-project/backend/server.js
+++ backend-project/backend/server.js
@@ -1,4 +1,26 @@
-import express from "express";
 
-const app = express();
+import {createServer} from "node:http";
+
 const PORT = 3000;
+
+const server = createServer((req,res)=>{
+    console.log("request recived");
+    res.statusCode= 200;
+    res.setHeader("Content-Type","text/html");
+    res.end(`
+    <!DOCTYPE html>
+    <html>
+      <head>
+        <title>Server Node.js</title>
+      </head>
+      <body>
+        <h1>Ciao!</h1>
+        <p>Questo è il mio messaggio personalizzato dal server Node.js.</p>
+      </body>
+    </html>
+  `);
+})
+
+server.listen(PORT, () => {
+  console.log(`Server in esecuzione su http://localhost:${PORT}`);
+});
