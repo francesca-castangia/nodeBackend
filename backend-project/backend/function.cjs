7ce478c234abd4fcdfb52977fca05e55e1f7c85b
francesca.castangia <castangiaf2@gmail.com>
2025-08-25 09:59:14 +0200

    esercizio Create and use CommonJS modules

---
+++ backend-project/backend/function.cjs
@@ -0,0 +1,7 @@
+
+function saluta(nome) {
+  return `Ciao, ${nome}!`;
+}
+
+
+module.exports = saluta;
---
+++ backend-project/backend/index.cjs
@@ -0,0 +1,7 @@
+
+
+// Importa la funzione dall'altro file
+const saluta = require('./function.cjs');
+
+// Chiama la funzione importata
+console.log(saluta('Mario'));
