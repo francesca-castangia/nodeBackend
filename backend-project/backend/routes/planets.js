7d5a52a1916422189fd666326be8e501a0d7f91b
francesca.castangia <castangiaf2@gmail.com>
2025-09-04 18:46:29 +0200

    progetto pianeti

---
+++ backend-project/backend/routes/planets.js
@@ -0,0 +1,5 @@
+import {Router} from "express";
+import Joi from "joi";
+
+const router = Router();
+const {object, string} = Joi;
--- backend-project/backend/server.js
+++ backend-project/backend/server.js
@@ -1,4 +1,52 @@
-import express from "express";
+
+
+let planets =[
+    { id: 1, name: "Hearth"},
+{ id: 2, name: "Mars"},
+];
+
+import "dotenv/config";
+import "express-async-errors";
+import pgPromise from "pg-promise";
+import morgan from "morgan";
+import planetsRouter from "./routes/planets";
+import express, {json} from "express";
 
 const app = express();
-const PORT = 3000;
+
+const db= pgPromise({})("postgres://postgres:Ruinedda2014!@localhost:5432/pianeti");
+
+console.log(db);
+
+const creaTabella = async()=>{
+    await db.none(`
+        CREATE TABLE IF NOT EXIST pianeti(
+        id SERIAL PRIMARY KEY ,
+        nome TEXT NOT NULL
+        )
+        `)
+
+        console.log("Tabella creata correttamente");
+}
+
+const popolaPianeti = async()=>{
+    await db.none(`
+        INSERT INTO pianeti
+        VALUES($1),($2)
+        `, ["Marte", "Terra"])
+        console.log("Avvenuto caricamento")
+}
+
+creaTabella().catch(console.error);
+popolaPianeti().catch(console.error);
+
+app.use(json());
+app.use(morgan("dev"));
+
+app.use("/api/planets", planetsRouter);
+
+app.use((err, req, res, next)=>{
+    console.error(err);
+    res.status(500).json({msg: "innternal server error"});
+});
+
